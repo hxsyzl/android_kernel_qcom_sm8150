27d6a68d64b2460fc255614d673956b375a760ae
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2026-01-25 14:32:31 +0800

    xsk: Treat `__poll_t` type as `unsigned int` type
    * We don't have `__poll_t` on kernel 4.14.
    
    Signed-off-by: Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>

--- net/xdp/xsk.c
+++ net/xdp/xsk.c
@@ -32,6 +32,12 @@
 #define TX_BATCH_SIZE 16
 
 static DEFINE_PER_CPU(struct list_head, xskmap_flush_list);
+typedef unsigned int __poll_t;
+
+static struct xdp_sock *xdp_sk(struct sock *sk)
+{
+	return (struct xdp_sock *)sk;
+}
 
 bool xsk_is_setup_for_bpf_map(struct xdp_sock *xs)
 {
